d3444d479d7da013206546563dcd381edee2d1fe
Sean McBride <spmcbride1201@gmail.com>
2019-04-13 09:08:15 -0400

    feat: complete maze kata

--- src/main/java/graphs/Maze.java
+++ src/main/java/graphs/Maze.java
@@ -1,5 +1,9 @@
 package graphs;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 // A maze will be given by a 2d integer matrix(int [][]) that only contains 1s
 // and 0s. 1 indicates a block that you cannot pass and 0 indicates a clear
 // space that you can pass from.
@@ -9,7 +13,75 @@ package graphs;
 // start point (1, 1) and the end point (1, 8), your program outputs ‘YES’ if
 // there exists at least one clear path between them and ‘NO’ if no path exists.
 public class Maze {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    Boolean visited[][] = {};
+    Boolean traversable[][] = {};
+    int[][] offsets = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+    public Maze(int pattern[][]) {
+        this.traversable = new Boolean[pattern.length][pattern[0].length];
+        this.visited = new Boolean[pattern.length][pattern[0].length];
+        for (int x = 0; x < pattern.length; x++) {
+            for (int y = 0; y < pattern[x].length; y++) {
+                boolean isTraversable = pattern[x][y] == 0;
+                this.traversable[x][y] = isTraversable;
+                this.visited[x][y] = false;
+            }
+        }
     }
+
+    private boolean isValid(int[] coordinates) {
+        int x = coordinates[0];
+        int y = coordinates[1];
+        return x >= 0 && x < this.traversable.length && y >= 0 && y < this.traversable[x].length;
+    }
+
+    private boolean isNotVisited(int[] coordinates) {
+        int x = coordinates[0];
+        int y = coordinates[1];
+        boolean isVisited = this.visited[x][y];
+        return isVisited == false;
+    }
+
+    private boolean isTraversable(int[] coordinates) {
+        int x = coordinates[0];
+        int y = coordinates[1];
+        boolean isTraversable = this.traversable[x][y];
+        return isTraversable;
+    }
+
+    private Stream<int[]> getPossibleMoves(int[] coordinates) {
+        int x = coordinates[0];
+        int y = coordinates[1];
+        return Arrays.stream(this.offsets)
+
+                .map(offset -> {
+                    int[] result = { x + offset[0], y + offset[1] };
+                    return result;
+                })
+
+                .filter(coords -> this.isValid(coords))
+
+                .filter(coords -> this.isTraversable(coords))
+
+                .filter(coords -> this.isNotVisited(coords));
+    }
+
+    public boolean canTraverse(int[] start, int[] end) {
+        if (isValid(start) == false || isTraversable(start) == false || isValid(end) == false
+                || isTraversable(end) == false) {
+            return false;
+        }
+        int x = start[0];
+        int y = start[1];
+        // Base Case: At End
+        if (x == end[0] && y == end[1]) {
+            return true;
+        } else {
+            visited[x][y] = true;
+            return this.getPossibleMoves(start).map(coords -> this.canTraverse(coords, end))
+                    .collect(Collectors.toList()).contains(true);
+
+        }
+    }
+
 }
--- src/test/java/graphs/MazeTest.java
+++ src/test/java/graphs/MazeTest.java
@@ -1,15 +1,101 @@
 package graphs;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class MazeTest {
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void twoOpenCellsShouldBeTrue() {
+        int[][] mazePattern = { { 0, 0 } };
+        Maze myMaze = new Maze(mazePattern);
+        int[] start = { 0, 0 };
+        int[] end = { 0, 1 };
+        assertTrue(myMaze.canTraverse(start, end));
+    }
+
+    @Test
+    public void twoClosedCellsShouldBeFalse() {
+        int[][] mazePattern = { { 1, 1 } };
+        Maze myMaze = new Maze(mazePattern);
+        int[] start = { 0, 0 };
+        int[] end = { 0, 1 };
+        assertFalse(myMaze.canTraverse(start, end));
+    }
+
+    @Test
+    public void middleCellBlocksShouldBeFalse() {
+        int[][] mazePattern = { { 0, 1, 0 } };
+        Maze myMaze = new Maze(mazePattern);
+        int[] start = { 0, 0 };
+        int[] end = { 0, 2 };
+        assertFalse(myMaze.canTraverse(start, end));
+    }
+
+    @Test
+    public void longOpenCorridorShouldBeTrue() {
+        int[][] mazePattern = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+        Maze myMaze = new Maze(mazePattern);
+        int[] start = { 0, 9 };
+        int[] end = { 0, 0 };
+        assertTrue(myMaze.canTraverse(start, end));
+    }
+
+    @Test
+    public void firstMaze() {
+        int[][] mazePattern = {
+
+                { 0, 0, 0, 1, 0 },
+
+                { 1, 0, 0, 1, 1 },
+
+                { 0, 0, 0, 1, 0 },
+
+                { 1, 0, 0, 0, 0 },
+
+                { 0, 0, 1, 0, 0 }
+
+        };
+        Maze myMaze = new Maze(mazePattern);
+        int[] start = { 0, 0 };
+        int[] end = { 4, 4 };
+        assertTrue(myMaze.canTraverse(start, end));
+        Maze myMaze2 = new Maze(mazePattern);
+        int[] start2 = { 0, 4 };
+        int[] end2 = { 4, 0 };
+        assertFalse(myMaze2.canTraverse(start2, end2));
+
+    }
+
+    @Test
+    public void secondMaze() {
+        int[][] mazePattern =
+
+                {
+
+                        { 0, 0, 0, 1, 0 },
+
+                        { 1, 0, 0, 1, 1 },
+
+                        { 0, 0, 0, 1, 0 },
+
+                        { 1, 0, 1, 0, 0 },
+
+                        { 0, 0, 1, 0, 0 }
+
+                };
+        Maze myMaze = new Maze(mazePattern);
+        int[] start = { 0, 0 };
+        int[] end = { 4, 4 };
+        assertFalse(myMaze.canTraverse(start, end));
+        Maze myMaze2 = new Maze(mazePattern);
+        int[] start2 = { 0, 4 };
+        int[] end2 = { 4, 0 };
+        assertFalse(myMaze2.canTraverse(start2, end2));
+        Maze myMaze3 = new Maze(mazePattern);
+        int[] start3 = { 0, 0 };
+        int[] end3 = { 4, 0 };
+        assertTrue(myMaze3.canTraverse(start3, end3));
     }
 }
